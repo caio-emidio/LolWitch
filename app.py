d2fedaa5947c7181ebe429a7600872b9e0f0d308
Caio Carnelos <caio.emidio@gmail.com>
2021-01-28 23:14:51 +0000

    Included Index and some improvements

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 import requests
+from decouple import config
 app = Flask(__name__)
 
 links = {
     "BR1": "br1.api.riotgames.com",
-    "EUN1": "eun1.api.riotgames.com"
+    "EUN1": "eun1.api.riotgames.com",
+    "EUW1": "euw1.api.riotgames.com",
+    "JP1": "jp1.api.riotgames.com",
+    "KR": "kr.api.riotgames.com",
+    "LA1": "la1.api.riotgames.com",
+    "LA2": "la2.api.riotgames.com",
+    "NA1": "na1.api.riotgames.com",
+    "OC1": "oc1.api.riotgames.com",
+    "TR1": "tr1.api.riotgames.com",
+    "RU": "ru.api.riotgames.com",
 }
 
 images = {
@@ -20,14 +30,23 @@ images = {
     "BRONZE": "Emblem_Bronze.png",
 }
 
-@app.route("/<country>/<name>/<type>")
-def index(country, name, type):
-    data = define_map(get(country, name))
-    return render_template('template.html', data = data[type])
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/rankedwitch/")
+def showData():
+    region = request.args.get('region')
+    name = request.args.get('summoner')
+    queue = request.args.get('queue')
+    color = request.args.get('color')
+    data = define_map(get(region, name))
+    return render_template('template.html', data = data[queue], color = color)
 
 
 def get(country, name):
-    headers = {'X-Riot-Token': 'RGAPI-68de7c3b-780b-4b87-b1de-2eafe71442d5'}
+    token = config('TOKEN')
+    headers = {'X-Riot-Token': token}
     url = f"https://{links[country]}/lol/summoner/v4/summoners/by-name/{name}"
     r = requests.get(url, headers=headers).json()
     id = r["id"]
@@ -45,7 +64,6 @@ def define_map(response):
                 "percentWin": float(res["wins"] / res["wins"] + res["losses"])
             }
         lista[res["queueType"]] = map
-    print(lista)
     return lista
 
 if __name__ == '__main__':
