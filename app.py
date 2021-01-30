300513e87c4af46e33ad84d3eb7fc6313a280c87
Caio Carnelos <caio.emidio@gmail.com>
2021-01-30 00:58:31 +0000

    Handled Errors and input Unranked

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ images = {
     "DIAMOND": "Emblem_Diamond.png",
     "CHALLENGER": "Emblem_Challenger.png",
     "BRONZE": "Emblem_Bronze.png",
+    "UNRANKED": "Emblem_Unranked.png",
 }
 
 @app.route("/")
@@ -40,34 +41,52 @@ def showData():
     name = request.args.get('summoner')
     queue = request.args.get('queue')
     color = request.args.get('color')
-    data = define_map(get(region, name))
+    data = define_map(get(region, name), queue)
     return render_template('template.html', data = data[queue], color = color)
 
 
-def get(country, name):
+def requestLink(url):
     token = config('TOKEN')
     headers = {'X-Riot-Token': token}
+    try:
+        return requests.get(url, headers=headers).json()
+    except ValueError:
+        return "No name recognized"
+def get(country, name):
     url = f"https://{links[country]}/lol/summoner/v4/summoners/by-name/{name}"
-    r = requests.get(url, headers=headers).json()
-    id = r["id"]
+    response = requestLink(url)
+    id = response["id"]
     url = f"https://{links[country]}/lol/league/v4/entries/by-summoner/{id}"
-    response = requests.get(url, headers=headers)
-    return response.json()
+    response = requestLink(url)
+    return response
 
-def define_map(response):
+def define_map(response, queue):
     lista = {}
-    for res in response:
+    if(len(response) > 0):
+        for res in response:
+            map = {
+                    "tier": images[res["tier"]],
+                    "rank": res["rank"],
+                    "leaguePoints": res["leaguePoints"],
+                    "percentWin": float(res["wins"] / res["wins"] + res["losses"]),
+                    "wins": res["wins"],
+                    "loses": res["losses"]
+                    
+                }
+            lista[res["queueType"]] = map
+    if queue not in lista:
         map = {
-                "tier": images[res["tier"]],
-                "rank": res["rank"],
-                "leaguePoints": res["leaguePoints"],
-                "percentWin": float(res["wins"] / res["wins"] + res["losses"]),
-                "wins": res["wins"],
-                "loses": res["losses"]
-                
-            }
-        lista[res["queueType"]] = map
+            "tier": images["UNRANKED"],
+            "rank": "Unranked",
+            "leaguePoints": 0,
+            "percentWin": 0,
+            "wins": 0,
+            "loses": 0       
+        }
+        lista[queue] = map
+
     return lista
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    DEBUG = config('DEBUG', default=False, cast=bool)
+    app.run(port=5000, debug=DEBUG)
